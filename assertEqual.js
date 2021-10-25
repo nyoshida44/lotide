c2c61917efb2f3c6f5fd0ac6121ae8bddf869e90
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-25 20:15:29 +0000

    Created assertEqual.js for lotide

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀😀😀 Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("💩💩💩 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Banana", "Banana");
+assertEqual(1, 1);
+assertEqual(5, 10);
