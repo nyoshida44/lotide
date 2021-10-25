0684605e3336a6254955e05146b14b0f6cd89747
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-25 20:34:14 +0000

    Added template literals to assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😀😀😀 Assertion Passed: " + actual + " === " + expected);
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("💩💩💩 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
