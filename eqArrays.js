e6211189d01c1039f3f0611675cd1d69d204fcfe
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-26 21:55:52 +0000

    eqArrays.js now works.

--- eqArrays.js
+++ eqArrays.js
@@ -7,5 +7,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// eqArrays Function
+const eqArrays = function(arrayCheck, arrayWant) {
+  let arrayResult = false;
+  for (i = 0; i < arrayCheck.length; i++) {
+    console.log(arrayCheck[i]);
+    console.log(arrayWant[i]);
+    if (arrayCheck[i] === arrayWant[i]) arrayResult = true;
+    else arrayResult = false;
+  }
+  return arrayResult;
+};
+
 // Tests
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
