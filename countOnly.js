ce365f8852068515852d1960ce44092a373d25a2
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-27 19:39:21 +0000

    Went through countOnly walkthrough, added countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -9,8 +9,18 @@ const assertEqual = function(actual, expected) {
 
 // countOnly Function
 const countOnly = function(allItems, itemsToCount) {
-
-}
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
 
 // Test
 const firstNames = [
