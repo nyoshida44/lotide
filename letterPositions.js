31d3dd52a1ce1fa853763e8af9b6be965ca4c919
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-27 23:24:05 +0000

    letterPositions.js works and tests have been added

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+// eqArrays Function
+const eqArrays = function(arrayCheck, arrayWant) {
+  let arrayResult = false;
+  for (let i = 0; i < arrayCheck.length; i++) {
+    if (arrayCheck[i] === arrayWant[i]) arrayResult = true;
+    else return false;
+  }
+  return arrayResult;
+};
+
+// assertArraysEqual Function
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    console.log(`😀😀😀 Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else console.log(`💩💩💩 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [];
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+// tests
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
