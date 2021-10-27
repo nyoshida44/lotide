a022277e09645dac2ee9ac5634139798d8b2402e
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-27 02:54:42 +0000

    Without.js finally completed. Added comments and tests.

--- without.js
+++ without.js
@@ -19,16 +19,35 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
 
 const without = function(arraySource, arrayRemove) {
   let withoutArray = [];
-  console.log(eqArrays(arraySource, arrayRemove));
   if (eqArrays(arraySource, arrayRemove) === false) {
-    for (let i = 0; i < arraySource.length; i++) {
-      if (arraySource[i] !== arrayRemove[i]) {
-        withoutArray.push(arraySource[i]);
+    // First for loop selects each value at arraySource.
+    for (let i = 0; i < arraySource.length; i++) { 
+      // Count crucial to see if all values have been compared in loops
+      let count = 0;
+      // Second for loop selects each value at arrayRemove.
+      for (let j = 0; j < arrayRemove.length; j++) {
+        // If statement to see if value in arraySource is not the same as the value in arrayRemove
+        if (arraySource[i] !== arrayRemove[j]) {
+          // Counts for every not same value
+          count += 1
+          // IMPORTANT: Count will me maxed out or same as arrayRemove.length if all values are 
+          // not the same. This means that all arrayRemove values do not match and as a result
+          // that arraySource value is approved to be pushed.
+          if (count === arrayRemove.length) {
+            withoutArray.push(arraySource[i]);
+          }
+        }
       }
     }
   }
   return withoutArray;
 };
 
+// Tests
 console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without([12, 2, 7, 6, 2, 4, "2", 12], [12, "2"]));
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
