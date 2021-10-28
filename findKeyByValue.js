d98f5ec7fbfb397e8eb37f24a3ee8cf8fb011c23
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-28 00:00:15 +0000

    Fixed eslint errors on findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,12 +9,12 @@ const assertEqual = function(actual, expected) {
 
 const findKeyByValue = function(scanObject, valueLookup) {
   for (const genre in scanObject) {
-    if (scanObject[genre] === valueLookup) return genre;  
+    if (scanObject[genre] === valueLookup) return genre;
   }
 };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  scifi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
@@ -23,4 +23,4 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "scifi");
