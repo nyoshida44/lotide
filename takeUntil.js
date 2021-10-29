79448f956fef546a64a4b2f75467fb40941c152c
Nicholas Yoshida <nyoshida44@gmail.com>
2021-10-29 02:39:07 +0000

    Added test and eslint modified takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,8 +1,25 @@
+// eqArrays Function
+const eqArrays = function(arrayCheck, arrayWant) {
+  let arrayResult = false;
+  for (let i = 0; i < arrayCheck.length; i++) {
+    if (arrayCheck[i] === arrayWant[i] && arrayCheck.length === arrayWant.length) arrayResult = true;
+    else return false;
+  }
+  return arrayResult;
+};
+
+// assertArraysEqual Function
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    console.log(`😀😀😀 Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else console.log(`💩💩💩 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+};
+
 const takeUntil = function(array, callback) {
   let emptyArray = [];
   for (const arr of array) {
-    if(callback(arr) === false) emptyArray.push(arr);
-    else if(callback(arr) === true)  return emptyArray;
+    if (callback(arr) === false) emptyArray.push(arr);
+    else if (callback(arr) === true)  return emptyArray;
   }
   return emptyArray;
 };
@@ -10,9 +27,11 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
