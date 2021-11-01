9d5d9c82c837ba6808ce7ee01cd772772402e3d0
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-01 18:07:32 +0000

    Got rid of require assertEqual line as assertEqual only needed for tests

--- head.js
+++ head.js
@@ -1,6 +1,3 @@
-// require the assertEqual function
-const assertEqual = require('./assertEqual');
-
 // head function
 const head = function(array) {
   return array[0];
--- tail.js
+++ tail.js
@@ -1,6 +1,3 @@
-// require the assertEqual function
-const assertEqual = require('./assertEqual');
-
 // tail Function
 const tail = function(array) {
   return array.slice(1);
